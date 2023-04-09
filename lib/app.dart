55f72b74cc882705e116b95bfd0b45b9df5b3bb3
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-04-10 01:25:00 +0600

    refactor(refactor): :recycle: initial refactoring

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Questloco',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(title: 'Home'),
+    );
+  }
+}
